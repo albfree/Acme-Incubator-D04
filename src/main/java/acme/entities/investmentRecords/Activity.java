20edc5d99fd6e38de485f28300d96405670340e5
Alberto Guerrero <albergr1197@gmail.com>
2020-08-06 21:08:27 +0200

    Se crea la entidad Investment Round

--- src/main/java/acme/entities/investmentRecords/Activity.java
+++ src/main/java/acme/entities/investmentRecords/Activity.java
@@ -4,12 +4,14 @@ package acme.entities.investmentRecords;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import acme.entities.investmentRounds.InvestmentRound;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
@@ -44,5 +46,10 @@ public class Activity extends DomainEntity {
 	//Derived attributes -----------------------------------------------
 
 	//Relationships ----------------------------------------------------
+
+	@ManyToOne(optional = false)
+	@NotNull
+	@Valid
+	private InvestmentRound		investor;
 
 }
